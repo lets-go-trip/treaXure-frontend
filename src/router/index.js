878840e9dc7462bbc374f63ba5e64de89d0d528e
nodb <scnoh02@naver.com>
2025-05-14 17:55:11 +0900

    feat[#5]: 로그인, 로그아웃 페이지 추가가

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,61 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'Treasure',
-    component: () => import('../views/Treasure.vue')
+    path: "/",
+    name: "Treasure",
+    component: () => import("../views/Treasure.vue"),
   },
   {
-    path: '/mission-list',
-    name: 'MissionList',
-    component: () => import('../views/MissionList.vue')
+    path: "/mission-list",
+    name: "MissionList",
+    component: () => import("../views/MissionList.vue"),
   },
   {
-    path: '/mission-list/:id',
-    name: 'MissionListById',
-    component: () => import('../views/MissionList.vue')
+    path: "/mission-list/:id",
+    name: "MissionListById",
+    component: () => import("../views/MissionList.vue"),
   },
   {
-    path: '/mission-detail',
-    name: 'MissionDetail',
-    component: () => import('../views/MissionDetail.vue')
+    path: "/mission-detail",
+    name: "MissionDetail",
+    component: () => import("../views/MissionDetail.vue"),
   },
   {
-    path: '/mission-detail/:id',
-    name: 'MissionDetailById',
-    component: () => import('../views/MissionDetail.vue')
+    path: "/mission-detail/:id",
+    name: "MissionDetailById",
+    component: () => import("../views/MissionDetail.vue"),
   },
   {
-    path: '/all-missions',
-    name: 'AllMissions',
-    component: () => import('../views/AllMissions.vue')
+    path: "/all-missions",
+    name: "AllMissions",
+    component: () => import("../views/AllMissions.vue"),
   },
   {
-    path: '/weekly-best',
-    name: 'WeeklyBest',
-    component: () => import('../views/WeeklyBest.vue')
+    path: "/weekly-best",
+    name: "WeeklyBest",
+    component: () => import("../views/WeeklyBest.vue"),
   },
   {
-    path: '/mypage',
-    name: 'MyPage',
-    component: () => import('../views/MyPage.vue')
-  }
-]
+    path: "/mypage",
+    name: "MyPage",
+    component: () => import("../views/MyPage.vue"),
+  },
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: () => import("../views/SignIn.vue"),
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: () => import("../views/SignUp.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router 
+export default router;
